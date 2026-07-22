a6231264bdd919d7b9975b764dda8707fab8a45b
lucapuie05 <puieluca05@gmail.com>
2026-07-22 21:20:10 +0300

    Implement Card Class with enum Rank and Suit

---
+++ src/Card.java
@@ -0,0 +1,23 @@
+public class Card {
+
+    final private Suit suit;
+    final private Rank rank;
+
+    public Card(Suit suit, Rank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit(){
+        return this.suit;
+    }
+
+    public Rank getRank(){
+        return this.rank;
+    }
+
+    @Override
+    public String toString(){
+        return this.rank+ " of " + this.suit;
+    }
+}
