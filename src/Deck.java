72c4168d54dcbe35a43cb3d686518e46a6d6a4e2
lucapuie05 <puieluca05@gmail.com>
2026-07-27 13:37:16 +0300

    Implement Deck creation and shuffle

--- src/Deck.java
+++ src/Deck.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Deck {
+
+    private Deque<Card> deck = new ArrayDeque<>();
+
+
+    public Deck(){
+
+        Suit[] suit = Suit.values();
+        Rank[] rank = Rank.values();
+
+        for(int i = 0; i < suit.length; i++){
+            for(int j = 0; j < rank.length; j++){
+                deck.push(new Card(suit[i], rank[j]));
+            }
+        }
+    }
+
+
+    public void shuffle(){
+
+        List<Card> cardList = new ArrayList<>(deck);
+        Collections.shuffle(cardList);
+        deck.clear();
+        for(int i = 0; i < cardList.size(); i++){
+            deck.push(cardList.get(i));
+        }
+    }
+
+
+    public boolean isEmpty(){
+        return deck.isEmpty();
+    }
+
+
+    public Card drawCard(){
+        return deck.removeFirst();
+    }
+
+
+    public int remainingCards(){
+        return deck.size();
+    }
+
 }
