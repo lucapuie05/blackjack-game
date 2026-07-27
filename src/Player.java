27f80a80e586348b8b0f09c6a61960fcc3486c8c
Luca Puie <e12416320@student.tuwien.ac.at>
2026-07-27 23:33:02 +0200

    Implement score calculation for a player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,33 @@
+import java.util.*;
+
 abstract public class Player {
 
-    
+    private List<Card> hand = new ArrayList<>();
+
+    public void addCard(Card card){
+        hand.add(card);
+    }
+
+    public int calculateScore(){
+
+        int score = 0;
+        int count = 0;
+
+        for(int i = 0; i < hand.size(); i++){
+            Card currentCard = hand.get(i);
+
+            if(!(currentCard.getRank().getValue() == 11)){
+                score += currentCard.getValue();
+            }else if(currentCard.getRank().getValue() == 11 && score < 11){
+                score += 11;
+                count++;
+            }
+        }
+
+        while(score > 21 && count != 0){
+            score -= 10;
+            count--;
+        }
+        return score;
+    }
 }
