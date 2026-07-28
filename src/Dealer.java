b9bdaf6e940747ea48a3e3e8d0d6f99b5e01a5c9
Luca Puie <puieluca05@gmail.com>
2026-07-29 00:00:33 +0200

    Implement HumanPlayer and Dealer

--- src/Dealer.java
+++ src/Dealer.java
@@ -1,2 +1,3 @@
-public class Dealer {
+public class Dealer  extends Player{
+
 }
--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -1,2 +1,12 @@
 public class HumanPlayer extends Player{
+
+    private String name;
+
+    public HumanPlayer(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
