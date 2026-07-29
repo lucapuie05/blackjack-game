5dfdfeebee98762cf0677252a9986f8b377573cf
lucapuie05 <puieluca05@gmail.com>
2026-07-29 13:16:11 +0300

    Implement GameStatistics class

--- src/GameStatistics.java
+++ src/GameStatistics.java
@@ -1,2 +1,25 @@
 public class GameStatistics {
+
+    private int wins;
+    private int draws;
+    private int losses;
+
+
+    public void addWin(){
+        wins++;
+    }
+
+    public void addDraw(){
+        draws++;
+    }
+
+    public void addLoss(){
+        losses++;
+    }
+
+    @Override
+    public String toString(){
+        return "You have " + wins + " wins, " + draws + " draws and " + losses + " losses";
+    }
+
 }
