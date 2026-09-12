093939cc85f56c89119b7868aa68f5171f8be69f
lucapuie05 <puieluca05@gmail.com>
2026-09-12 16:24:16 +0300

    Refactor Game to delegate UI output to ConsoleUI

--- src/Game.java
+++ src/Game.java
@@ -3,31 +3,20 @@ import java.util.Scanner;
 public class Game {
 
     private GameStatistics statistics = new GameStatistics();
-    private Scanner sc = new Scanner(System.in);
+    private ConsoleUI ui = new ConsoleUI();
 
     public void startGame(){
 
-        System.out.println("===================================");
-        System.out.println("BLACKJACK");
-        System.out.println("===================================");
-
-        System.out.println();
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.println("Welcome to BLACKJACK " + name);
-        System.out.println();
+        ui.start();
+        String name = ui.askName();
+        ui.greetPlayer(name);
 
         int count = 0;
 
         do{
 
             count++;
-
-            System.out.println("===================================");
-            System.out.println("ROUND " + count);
-            System.out.println("===================================");
-            System.out.println();
+            ui.showRound(count);
 
             Deck deck = new Deck();
             deck.shuffle();
@@ -37,23 +26,26 @@ public class Game {
 
             drawFirstTwoCards(player,dealer,deck);
             if(checkBlackjack(player) && !checkBlackjack(dealer)){
-                System.out.println("BLACKJACK! You won!");
+                ui.showBlackjackResult("BLACKJACK! You won!");
                 statistics.addWin();
                 if(!endRound(name)){
                     return;
                 }
+
             }else if(checkBlackjack(player) && checkBlackjack(dealer)){
-                System.out.println("You both have BLACKJACK! Draw!");
+                ui.showBlackjackResult("You both have BLACKJACK! Draw!");
                 statistics.addDraw();
                 if(!endRound(name)){
                     return;
                 }
+
             }else if(!checkBlackjack(player) && checkBlackjack(dealer)){
-                System.out.println("Dealer has BLACKJACK! You lost!");
+                ui.showBlackjackResult("Dealer has BLACKJACK! You lost!");
                 statistics.addLoss();
                 if(!endRound(name)){
                     return;
                 }
+
             } else {
                 boolean playerBust = playerTurn(player,deck);
                 if(playerBust){
@@ -63,8 +55,7 @@ public class Game {
                 }else {
                     boolean dealerBust = dealerTurn(dealer, deck);
                     if(dealerBust){
-                        System.out.println("Dealer bust! You won!");
-                        System.out.println("Dealer score:" + dealer.calculateScore());
+                        ui.dealerBust(dealer.calculateScore());
                         if(!endRound(name)){
                             return;
                         }
@@ -76,9 +67,6 @@ public class Game {
                     }
                 }
             }
-
-
-
         }while (true);
 
     }
@@ -88,20 +76,23 @@ public class Game {
 
 
     private void calculateWinner(Player player, Player dealer){
-        if(player.calculateScore() > dealer.calculateScore()){
-            System.out.println("You won!");
-            System.out.println("Your score:" + player.calculateScore());
-            System.out.println("Dealer score:" + dealer.calculateScore());
+
+        String flag;
+        int playerScore = player.calculateScore();
+        int dealerScore = dealer.calculateScore();
+        if(playerScore > dealerScore){
+            flag = "Player";
+            ui.showWinner(flag, playerScore, dealerScore);
             statistics.addWin();
-        }else if(player.calculateScore() < dealer.calculateScore()){
-            System.out.println("You lost! Dealer won!");
-            System.out.println("Dealer score: " + dealer.calculateScore());
-            System.out.println("Your score:" + player.calculateScore());
+
+        }else if(playerScore < dealerScore){
+            flag = "Dealer";
+            ui.showWinner(flag, playerScore, dealerScore);
             statistics.addLoss();
+
         }else {
-            System.out.println("Draw!");
-            System.out.println("Your score:" + player.calculateScore());
-            System.out.println("Dealer score:" + dealer.calculateScore());
+            flag = "Draw";
+            ui.showWinner(flag, playerScore, dealerScore);
             statistics.addDraw();
         }
     }
@@ -109,11 +100,10 @@ public class Game {
 
     private void drawFirstTwoCards(Player player, Player dealer, Deck deck){
 
-        System.out.print("Your cards are: ");
         for(int i = 0; i < 2; i++) {
 
             Card playerCard = deck.drawCard();
-            System.out.print(playerCard.toString() + "  ");
+            ui.showPlayerCard(playerCard.toString());
             player.addCard(playerCard);
 
 
@@ -121,8 +111,8 @@ public class Game {
 
             dealer.addCard(dealerCard);
         }
-        System.out.println();
-        System.out.println("Dealer first card is: " + dealer.getCard(0));
+
+        ui.showDealerFirstCard(dealer.getCard(0).toString());
     }
 
 
@@ -130,28 +120,25 @@ public class Game {
 
         while(player.calculateScore() < 21){
 
-            System.out.println("Your score is: " + player.calculateScore());
+            ui.showScore(player.calculateScore());
             String answer;
 
             do{
-                System.out.println("Hit or Stand? ");
-                answer = sc.nextLine().toLowerCase();
+                answer = ui.askHitOrStand();
 
                 if (!(answer.equals("hit") || answer.equals("stand"))) {
-                    System.out.println("Invalid input! Please enter 'hit' or 'stand'.");
+                    ui.errorInputAskCard();
                 }
-
             }while(!(answer.equals("hit") || answer.equals("stand")));
 
 
 
             if(answer.equals("hit")){
                 Card playerCard = deck.drawCard();
-                System.out.println(playerCard.toString());
+                ui.showPlayerCard(playerCard.toString());
                 player.addCard(playerCard);
                 if(player.calculateScore() > 21){
-                    System.out.println("BUST! You lost!");
-                    System.out.println("Your score is: " + player.calculateScore());
+                    ui.showPlayerBust(player.calculateScore());
                     statistics.addLoss();
                     return true;
                 }
@@ -165,12 +152,12 @@ public class Game {
 
     private boolean dealerTurn(Player dealer, Deck deck){
 
-        System.out.println("Dealer second card is: " + dealer.getCard(1));
+        ui.showDealerSecondCard(dealer.getCard(1).toString());
 
         while(dealer.calculateScore() < 17){
             Card dealerCard = deck.drawCard();
             dealer.addCard(dealerCard);
-            System.out.println("Dealer draws: " + dealerCard.toString());
+            ui.showDealerDraws(dealerCard.toString());
             if(dealer.calculateScore() > 21){
                 statistics.addWin();
                 return true;
@@ -185,11 +172,10 @@ public class Game {
 
         String answer;
         do{
-            System.out.println("Do you want to play again? YES or NO?");
-            answer = sc.nextLine().toLowerCase();
 
+            answer = ui.askPlayer();
             if(!(answer.equals("yes") || answer.equals("no"))){
-                System.out.println("Invalid input! Please enter 'yes' or 'no'.");
+                ui.errorInputAskGame();
             }
 
         }while (!(answer.equals("yes") || answer.equals("no")));
@@ -197,8 +183,7 @@ public class Game {
         if(answer.equals("yes")){
             return true;
         }else{
-            System.out.println("Thank you for participating " + name + "!");
-            System.out.println("GOODBYE!");
+            ui.showGoodbye(name);
             return false;
         }
     }
@@ -209,7 +194,7 @@ public class Game {
     }
 
     private boolean endRound(String name){
-        System.out.println(statistics.toString());
+        ui.showStatistics(statistics.toString());
         return askPlayer(name);
     }
 }
